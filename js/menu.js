96e15a28cc4974295eb175a52368b17e94a85c46
[jona0812] <jona0812>
2023-10-24 22:04:35 +0200

    I started to do responsive footer menu

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,21 @@
 (function() {
     
     const openButton=document.querySelector('.nav_menu');
+    const menu= document.querySelector('.nav_link');
+    const cerrarMenu= document.querySelector('.nav_close');
+
 
     openButton.addEventListener('click', ()=>{
-        
-        alert('Uated me acabó de dar click');
+        menu.classList.add('nav_link-show');
+        // alert('Uated me acabó de dar click');
     });
+
+    cerrarMenu.addEventListener('click', () => {
+        // alert('Uated me acabó de dar click');
+        
+        menu.classList.remove('nav_link-show');
+
+    });
+    // menu.classlist.toogle podría funcionar pero solo es para agregar si no la tiene y para quitar la clase si la tiene
 
 })();
